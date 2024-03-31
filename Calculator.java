0e6b77fc771c7ecc8267f232908046f718d69ef2
Khryz-Navarro <s.konavarro@usm.edu.ph>
2024-03-31 15:53:24 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -28,24 +28,24 @@ public class Calculator {
 
         switch (choice) {
             case 1:
-                JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: "+ "Addition" + "\nResult: " + add(num1,num2));    
+                JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: Addition" + "\nResult: " + add(num1,num2));    
                 break;
             case 2:
-                JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: "+ "Subtraction" + "\nResult: " + subtract(num1,num2));    
+                JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: Subtraction" + "\nResult: " + subtract(num1,num2));    
                 break;
             case 3:
-                JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: "+ "Multiplication" + "\nResult: " + multiply(num1,num2));    
+                JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: Multiplication" + "\nResult: " + multiply(num1,num2));    
                 break;
             case 4:
                 if (num2 != 0) {
-                    JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: "+ "Division" + "\nResult: " + divide(num1,num2));
+                    JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: Division" + "\nResult: " + divide(num1,num2));
                 }
                 else {
                     JOptionPane.showMessageDialog(null, "Cannot Divide By Zero");
                 }
                 break;
             case 5:
-                    JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: "+ "Modulus" + "\nResult: " + modulus(num1,num2));
+                    JOptionPane.showMessageDialog(null, "num1: "+ num1 + "\nnum2: "+ num2 + "\nOperation used: Modulus" + "\nResult: " + modulus(num1,num2));
                 break;
         
             default:
